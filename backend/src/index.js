b7c4c2dba8cd2705e5af32709ab667107d447d8d
vtorres96 <victorcdc96@gmail.com>
2020-06-10 00:58:45 -0300

    added application server

---
+++ backend/src/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const cors = require('cors');
+const http = require('http');
+
+const routes = require('./routes');
+
+const app = express();
+const server = http.Server(app);
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+server.listen(3000, () => {
+  console.log('Servidor rodando na porta: 3000');
+});
